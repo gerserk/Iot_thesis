7ccb7115b8b6989310d282dc5db2f656690ac9d2
gerserk <francescogagggini@gmail.com>
2023-04-25 10:17:21 +0000

    -

--- plugin/listnerer_plugin.py
+++ plugin/listnerer_plugin.py
@@ -1,12 +1,3 @@
-LATENCY= 5 # ascolta e manda ogni 5 secondi
-USER='dhadmin'
-PASSWORD='dhadmin_#911'
-AUTH_URL='http://francesco-legion-5-15ach6h/api/rest'
-URL='http://francesco-legion-5-15ach6h/plugin/proxy/'
-BROKER = 'francesco-legion-5-15ach6h'
-TOPIC_NAME='dh/request'
-TOKEN= 'eyJhbGciOiJIUzI1NiJ9.eyJwYXlsb2FkIjp7ImEiOlsxNl0sImUiOjE2ODIzNTQwMDk4MTksInQiOjEsInRwYyI6InBsdWdpbl90b3BpY19kZjEwYWI2OC04NjYyLTQ0ODItYWFjNC1jMzI3NWI4YTMzNGMifX0.gEzo-ZdfpfiG2cg7sN-A9jeEp84OHAAtlLip5wr98_s'
-
 import paho.mqtt.client as mqtt
 import uuid
 import json
@@ -14,7 +5,7 @@ import time
 from devicehive_plugin import Handler
 from devicehive_plugin import Plugin
 
-
+old=0
 class SimpleHandler(Handler):
     def handle_connect(self):
         print('Successfully connected')
@@ -30,8 +21,17 @@ class SimpleHandler(Handler):
         print(command.command)
 
     def handle_notification(self, notification): # will be called after notification/insert event is received
-        print(notification.notification)
+        print('notifica:\n'+ notification.notification)
+        print('timestamp:\n'+ notification.timestamp)
+        print('parametro:\n') 
+        print(notification.parameters)
+        
+
+url = 'ws://francesco-legion-5-15ach6h/plugin/proxy/'
+topic_name = 'plugin_topic_27e39a6e-5dac-4d36-83b3-d2785c92d7f9' 
+auth_url = 'http://francesco-legion-5-15ach6h/api/rest'
 
 plugin = Plugin(SimpleHandler)
-#plugin.connect(URL, TOPIC_NAME, auth_url=AUTH_URL, login=USER, password=PASSWORD) 
-plugin.connect(URL, TOPIC_NAME, plugin_access_token=TOKEN)
+# plugin.connect(url, topic_name, plugin_access_token=plugin_access_token)
+plugin.connect(url, topic_name, auth_url=auth_url,
+               login='dhadmin', password='dhadmin_#911')
